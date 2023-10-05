21993f30185b49dcf83341926d2a6b32dcd8ee23
smith <smithbabawande@gmail.com>
2023-10-05 16:22:09 +0100

    Completed Assignment

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    def __init__(self, name, breed = "Mutt"):
+        self.name = name
+        self.breed = breed
+        print(f"{name} is born!")
+   
+    def bark(self):
+        print("Woof!")
+    
+    def showing_self(self):
+        return self
+    
+fido = Dog("Fido")
+
+fido.showing_self()
+
+snoppy = Dog("Snoopy")
+
+fido.breed = "Dalmatian"
+snoppy.breed = "Beagle"
